31e6a8e4f2c0f45c5f5d1fd32d704b51328af96d
Sudhir Mishra <sudhirxps@gmail.com>
2016-10-02 05:06:21 +0530

    HotFix: Static dir changes

--- hoteldeals/settings.py
+++ hoteldeals/settings.py
@@ -141,13 +141,13 @@ STATICFILES_DIRS = [
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
